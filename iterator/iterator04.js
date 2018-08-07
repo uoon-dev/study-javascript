a3f322d82bf001a3990c53b3e77c3f0845c02b2b
uoon.dev <uoon.dev@gmail.com>
2018-08-07 18:45:58 +0900

    iterator with es6

--- iterator/iterator04.js
+++ iterator/iterator04.js
@@ -1,3 +1,28 @@
 /**
  * With ES2015 class
  */
+class SimpleClass {
+    constructor(data) {
+        this.index = 0;
+        this.data = data;
+    }
+
+    [Symbol.iterator]() {
+        return {
+            next: () => {
+                if (this.index < this.data.length) {
+                    return {value: this.data[this.index++], done: false}
+                } else {
+                    this.index = 0
+                    return {done: true}
+                }
+            }
+        }
+    }
+}
+
+const simple = new SimpleClass([1,2,3,4,5])
+
+for (const val of simple) {
+    console.log(val)
+}
