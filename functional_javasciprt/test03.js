a0f75d39a717e6a02602bc1e3da0111665b5eda9
uoon.dev <uoon.dev@gmail.com>
2018-10-17 18:18:49 +0900

    fix function

--- functional_javasciprt/test03.js
+++ functional_javasciprt/test03.js
@@ -1,12 +1,11 @@
 const input = [2, 1, 0, 3, 0, 5, 1, 4, 2, 0, 3];
-const result = [];
+const result = 0;
 const getShortestDuplicatedDate = (input) => input.map((value, index) => {
         const nextIndex = input.indexOf(value, index + 1);
         if (nextIndex < 0) return 0;
         return nextIndex - index;
     }).filter(val => val !== 0)
     // .reduce((acc, curr, index, arr) => (acc < curr) ? acc : curr);
-
-console.log(getShortestDuplicatedDate(input));
-
+result = Math.min(...getShortestDuplicatedDate(input));
+console.log(result);
  
